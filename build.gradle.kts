94fa28036b5129cb0941fe413599773731a85a96
jjunji <jh.jun92@gmail.com>
2024-06-13 17:31:30 +0900

    feat: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,28 @@ plugins {
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.detekt) apply false
 
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+}
+
+buildscript {
+    tasks.register("copyGitHooks", Copy::class.java) {
+        description = "Copies the git hooks from /git-hooks to the .git folder."
+        group = "git hooks"
+        from("$rootDir/scripts/pre-commit")
+        into("$rootDir/.git/hooks/")
+    }
+
+    tasks.register("installGitHooks", Exec::class.java) {
+        description = "Installs the pre-commit git hooks from /git-hooks."
+        group = "git hooks"
+        workingDir = rootDir
+        commandLine = listOf("chmod")
+        args("-R", "+x", ".git/hooks/")
+        dependsOn("copyGitHooks")
+        doLast {
+            logger.info("Git hook installed successfully.")
+        }
+    }
 }
