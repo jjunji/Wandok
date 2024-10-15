63bfdbcc8ac436efeb65ef14e5076517032574c7
jjunji <jh.jun92@gmail.com>
2024-10-15 17:26:48 +0900

    feat: draw progress sample

--- app/src/main/java/com/example/wandok/ui/home/MyProgressView.kt
+++ app/src/main/java/com/example/wandok/ui/home/MyProgressView.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -22,6 +23,15 @@ import com.example.wandok.common.extension.toPx
 import com.example.wandok.ui.core.ShadowContainer
 import com.example.wandok.ui.theme.Orange100
 import com.example.wandok.ui.theme.Orange800
+
+val frameWidth = 280.dp
+val frameHeight = 430.dp
+val frameRadius = 164.dp
+val progressWidth = 250.dp
+val progressHeight = 390.dp
+val progressRadius = 141.dp
+
+val strokeWidth = 8.dp
 
 @Composable
 fun MyOvalProgressView(
@@ -34,29 +44,25 @@ fun MyOvalProgressView(
         radius = 164.dp
     ) {
         BackgroundView {
-            MyOvalProgressBar(modifier = Modifier)
-            ArchSample()
+            MyOvalProgressBar(modifier = Modifier)  // custom progress view
+            ArchSample()                            //
         }
     }
 }
 
 @Composable
 fun MyOvalProgressBar(modifier: Modifier) {
-    val radius = 141.dp
-    val width = 250.dp
-    val height = 390.dp
-
     Canvas(
         modifier = modifier
-            .width(width)
-            .height(height)
+            .width(progressWidth)
+            .height(progressHeight)
     ) {
         drawRoundRect(
             color = Orange100,
-            cornerRadius = CornerRadius(radius.toPx(), radius.toPx()),
+            cornerRadius = CornerRadius(progressRadius.toPx(), progressRadius.toPx()),
             topLeft = Offset(0f, 0f),
-            size = Size(width.toPx(), height.toPx()),
-            style = Stroke(width = 8.dp.toPx())
+            size = Size(progressWidth.toPx(), progressHeight.toPx()),
+            style = Stroke(width = strokeWidth.toPx())
         )
     }
 }
@@ -68,9 +74,9 @@ fun BackgroundView(
 ) {
     Box(
         modifier = modifier
-            .width(280.dp)
-            .height(430.dp)
-            .background(shape = RoundedCornerShape(164.dp), color = Color.White),
+            .width(frameWidth)
+            .height(frameHeight)
+            .background(shape = RoundedCornerShape(frameRadius), color = Color.White),
         contentAlignment = Alignment.Center
     ) {
         content()
@@ -79,14 +85,17 @@ fun BackgroundView(
 
 @Composable
 fun ArchSample() {
-    val radius = 141.dp.toPx()
-    val frameWidth = 250.dp.toPx()
-    val frameHeight = 390.dp.toPx()
-    val straightLength = frameHeight - (2 * radius) // 직선의 길이 (세로 변에서 곡선이 아닌 영역)
+    val straightLength = progressWidth.toPx() - (2 * progressRadius.toPx()) // 직선의 길이 (세로 변에서 곡선이 아닌 영역)
+
+    val startOffset = Pair(progressWidth.toPx(), progressHeight.toPx() / 2)
+
+    val topRect = Rect(0f, 0f, startOffset.first, startOffset.second + straightLength / 2)
+    val bottomRect = Rect(0f, startOffset.second, startOffset.first, startOffset.second * 2)
 
     val path = Path().apply {
-        moveTo(frameWidth, frameHeight / 2)
-        lineTo(frameWidth, frameHeight / 2 - straightLength / 2)
+        moveTo(startOffset.first, startOffset.second + straightLength / 2)
+        arcTo(topRect, 0f, -180f, false)
+        arcTo(bottomRect, 0f, -180f, false)
     }
 
     Canvas(
@@ -111,4 +120,10 @@ fun PreviewOvalProgress() {
     ) {
         MyOvalProgressView(modifier = Modifier)
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewArchSample() {
+    ArchSample()
 }
